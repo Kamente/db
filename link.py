5bd8c22c32cb341a4033df23e89dc475b493ed4d
Kamente <justinm48000@gmail.com>
2023-08-28 10:17:23 +0300

    link db to python

---
+++ link.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+class Students:
+    def __init__(self, student_name, student_reg):
+        self.student_name = student_name
+        self.student_reg = student_reg
+        
+
+connection = sqlite3.connect('library.db')
+
+db_cursor = connection.cursor() # used to enable execution of the sql commands
+
+db_cursor.execute("SELECT * FROM books;")
+
+for book in db_cursor:
+    print (book)
